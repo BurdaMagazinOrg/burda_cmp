b682d30695dcb9ae6c73c13d7aaf51f48b8a9355
Thomas Keitel <tom@undpaul.de>
2020-07-30 17:23:14 +0200

    BHEALTH-1294: Add missing purposes for Google Maps embeds

--- src/StaticConsentData.php
+++ src/StaticConsentData.php
@@ -41,8 +41,9 @@ class StaticConsentData implements StaticConsentDataInterface {
           'label' => $this->t('Google Maps', [], ['context' => 'Cookie consent provider']),
           // @todo Toggle label should be translatable for Google Maps.
           'toggle_label' => 'Google Maps anzeigen',
-          // @todo Still needs correct purpose IDs for Google Maps.
-          'purposes' => [],
+          'purposes' => [
+            29,
+          ],
         ],
 
         // Instagram.
