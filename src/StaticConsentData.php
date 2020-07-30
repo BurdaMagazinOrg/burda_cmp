8d7b688cac771b698b6d71a97ba8c8689e7a4d9b
Thomas Keitel <tom@undpaul.de>
2020-07-30 17:23:06 +0200

    BHEALTH-1294: Add missing purposes for Vimeo video embeds

--- src/StaticConsentData.php
+++ src/StaticConsentData.php
@@ -107,8 +107,10 @@ class StaticConsentData implements StaticConsentDataInterface {
           'label' => $this->t('Vimeo', [], ['context' => 'Cookie consent provider']),
           // @todo Toggle label should be translatable for Vimeo videos.
           'toggle_label' => 'Vimeo-Videos anzeigen',
-          // @todo Still needs correct purpose IDs for Vimeo.
-          'purposes' => [],
+          'purposes' => [
+            26,
+            29,
+          ],
         ],
 
         // Xandr, Inc.
