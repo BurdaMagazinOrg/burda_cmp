55dd6a7443a2b3d3585c7e7d6821e838165d507e
Markus Bruns <bruns@kontrollfeld.de>
2023-10-25 16:41:03 +0200

    - added Settings Form Field to add sourcepoint scripts and refactored liveramp with sourcepoint - embedded scripts from SettingsForm

--- src/Form/SettingsForm.php
+++ src/Form/SettingsForm.php
@@ -32,10 +32,24 @@ class SettingsForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('burda_cmp.settings');
 
-    $form['liveramp_script_url'] = [
+    $form['sourcepoint_script_url'] = [
       '#type' => 'url',
-      '#title' => $this->t('LiveRamp script URL'),
-      '#default_value' => $config->get('liveramp_script_url'),
+      '#title' => $this->t('Sourcepoint script URL'),
+      '#default_value' => $config->get('sourcepoint_script_url'),
+      '#required' => TRUE,
+    ];
+
+    $form['sourcepoint_shim_script'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Sourcepoint Shim URL'),
+      '#default_value' => $config->get('sourcepoint_shim_script'),
+      '#required' => TRUE,
+    ];
+
+    $form['sourcepoint_config_code'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Sourcepoint Config Code'),
+      '#default_value' => $config->get('sourcepoint_config_code'),
       '#required' => TRUE,
     ];
 
@@ -47,7 +61,9 @@ class SettingsForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('burda_cmp.settings')
-      ->set('liveramp_script_url', $form_state->getValue('liveramp_script_url'))
+      ->set('sourcepoint_script_url', $form_state->getValue('sourcepoint_script_url'))
+      ->set('sourcepoint_shim_script', $form_state->getValue('sourcepoint_shim_script'))
+      ->set('sourcepoint_config_code', $form_state->getValue('sourcepoint_config_code'))
       ->save();
 
     parent::submitForm($form, $form_state);
